ba7bc50f8cd48ef2609e5badeba4312973740088
sharafath11 <abisharfath@gmail.com>
2025-03-08 12:30:11 +0530

    we are winners

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,21 +17,22 @@ const Footer = () => {
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your</span> digital
-          presence to the next level?
+          Ready to stand up for <span className="text-purple">education</span>{" "}
+          and change the world?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
+          Join the movement and be a voice for those who deserve the right to
+          learn.
         </p>
-        <a href="mailto:contact@jsmastery.pro">
+        <a href="https://malala.org">
           <MagicButton
-            title="Let's get in touch"
+            title="Support the Cause"
             icon={<FaLocationArrow />}
             position="right"
           />
         </a>
       </div>
+
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2025 siyad&sharafath
